158044b094bfe511fbcc546cbd0d45ecef9f4e0f
NFiveDev <nmhjort7@gmail.com>
2023-12-29 22:18:18 +0100

    prettier

--- components/themeToggle.tsx
+++ components/themeToggle.tsx
@@ -1,10 +1,11 @@
-'use client'
+'use client';
 
-import useTheme from "@/hooks/useTheme"
+import useTheme from '@/hooks/useTheme';
 import { HiSun, HiMoon } from 'react-icons/hi2';
 
 export function ThemeToggle() {
-  const { isLightMode, isDarkMode, enableDarkMode, disableDarkMode } = useTheme();
+  const { isLightMode, isDarkMode, enableDarkMode, disableDarkMode } =
+    useTheme();
 
   const onClickHandler = () => {
     if (isDarkMode()) {
@@ -12,10 +13,13 @@ export function ThemeToggle() {
     } else if (isLightMode()) {
       enableDarkMode();
     }
-  }
+  };
 
   return (
-    <button onClick={onClickHandler} className='mx-2 lg:mx-0 transition-all ease-in-out duration-300 dark:bg-gray-700 bg-gray-200 hover:bg-gray-400 cursor-pointer rounded-full h-fit p-1'>
+    <button
+      onClick={onClickHandler}
+      className='mx-2 lg:mx-0 transition-all ease-in-out duration-300 dark:bg-gray-700 bg-gray-200 hover:bg-gray-400 cursor-pointer rounded-full h-fit p-1'
+    >
       {isLightMode() && <HiSun className='text-yellow-400' />}
       {isDarkMode() && <HiMoon className='text-blue-600' />}
     </button>
@@ -23,5 +27,5 @@ export function ThemeToggle() {
     //   <input onClick={onClickHandler} type="checkbox" id="check" className="sr-only"/>
     //   <span className={`absolute h-full w-3/6 rounded-full dark:bg-slate-600  `}></span>
     // </label>
-  )
+  );
 }
