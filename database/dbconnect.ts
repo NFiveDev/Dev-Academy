60b47e51291a0c667be11b6174fcf1f3735c44ee
NFiveDev <nmhjort7@gmail.com>
2023-12-29 22:31:15 +0100

    ignore env

--- database/dbconnect.ts
+++ database/dbconnect.ts
@@ -1,3 +1,15 @@
 import mongoose from 'mongoose';
 
-console.log('hej');
+const ENV_CONNECTION_STRING = process.env.MONGO_URL;
+
+const connect = async () => {
+  if (!ENV_CONNECTION_STRING) throw Error('NO CONNECTION STRING SPECIFIED');
+
+  try {
+    await mongoose.connect(ENV_CONNECTION_STRING);
+  } catch (error) {
+    throw new Error('connection failed with err: ' + error);
+  }
+};
+
+export default connect;
