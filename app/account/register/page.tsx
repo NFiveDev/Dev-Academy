3d15afa53187fcc9726d0ad63bb57bae08e92f89
NFiveDev <nmhjort7@gmail.com>
2023-12-31 16:08:31 +0100

    styling mobile

--- app/account/register/page.tsx
+++ app/account/register/page.tsx
@@ -3,8 +3,8 @@ import SignUpForm from './signupform';
 
 export default function Register() {
   return (
-    <div className='relative m-auto py-36'>
-      <div className='bg-slate-700 py-4 lg:py-8 px-4 lg:px-10 min-w-60 rounded-lg max-w-screen-md border-emerald-600 border-2'>
+    <div className='relative m-auto py-10 lg:py-28'>
+      <div className='bg-white dark:bg-slate-700 py-4 lg:py-8 px-4 lg:px-10 w-64 lg:w-96 rounded-lg max-w-screen-md border-emerald-600 border-2'>
         <div className='flex w-fit mx-auto p-4 rounded-full border-emerald-600 border-2'>
           <HiRocketLaunch className='text-slate-300 w-14 h-14' />
         </div>
