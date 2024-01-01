b102a2867c4d346e25e553d70fa9b6ab37d53470
NFiveDev <nmhjort7@gmail.com>
2024-01-02 00:42:15 +0100

    zod

--- app/account/register/actions.ts
+++ app/account/register/actions.ts
@@ -2,10 +2,21 @@
 
 import { z } from 'zod';
 
+const createUserActionResponse = {
+  errorMessage: '',
+  StatusOK: true,
+};
+
 export default async function createUser(prevState: any, formData: FormData) {
-  const s = formData.get('email');
-  return {
-    errorMessage: '',
-    ok: true,
-  };
+  const email = formData.get('email');
+  const username = formData.get('username');
+  const password = formData.get('password');
+
+  if (!email || !username || !password) {
+    createUserActionResponse.StatusOK = false;
+    createUserActionResponse.errorMessage =
+      'You must provide username, email and password';
+  }
+
+  return createUserActionResponse;
 }
